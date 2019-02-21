b52c2d55828f53fc308188d626b7b7113a686e3b
Victor Zhuravlev <vzhuravlev15@gmail.com>
2019-02-22 01:35:12 +0300

    chore: add build with ts, react, emotion

--- webpack.config.js
+++ webpack.config.js
@@ -10,11 +10,7 @@ module.exports = {
     libraryTarget: 'commonjs'
   },
 
-  // Enable sourcemaps for debugging webpack's output.
-  // devtool: 'source-map',
-
   resolve: {
-    // Add '.ts' and '.tsx' as resolvable extensions.
     extensions: ['.ts', '.tsx', '.js', '.json']
   },
 
@@ -22,15 +18,16 @@ module.exports = {
 
   module: {
     rules: [
-      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-      // { test: /\.(ts|tsx)$/, loader: 'awesome-typescript-loader' },
       {
-        test: /\.tsx?$/,
+        test: /\.svg$/,
+        use: ['@svgr/webpack']
+      },
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
         loader: 'babel-loader'
       },
-
-      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-      { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' }
+      { enforce: 'pre', test: /\.(ts|js)x?$/, loader: 'source-map-loader' }
     ]
   }
 };
