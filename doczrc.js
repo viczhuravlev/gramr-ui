404e0e6648842a90e3a4e626250d95f2e40296ae
Victor Zhuravlev <vzhuravlev15@gmail.com>
2019-02-22 01:48:02 +0300

    chore: add and config docs

---
+++ doczrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  title: `Gramr`,
+  description: 'description',
+  src: './src',
+  typescript: true,
+  codeSandbox: true,
+  port: 8888,
+  themeConfig: {
+    colors: {
+      primary: '#03a9f4'
+    }
+  },
+  modifyBundlerConfig: (config, dev, args) => {
+    const svgIndex = config.module.rules.findIndex(elem => {
+      return String(elem.test) === String(/\.(svg)(\?.*)?$/);
+    });
+
+    config.module.rules.splice(svgIndex, 1);
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack']
+    });
+
+    return config;
+  }
+};
